7192c3e919ff0a31dcb0abd11705ad1d437df99f
Kostya <muhabrot@mail.ru>
2023-02-25 20:10:41 +0600

    Changed environ var name to correct

--- core/settings.py
+++ core/settings.py
@@ -68,7 +68,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 DB_NAME = os.environ.get('DB_NAME', '')
 DB_USER = os.environ.get('DB_USER', '')
-DB_PASSWORD = os.environ.get('DB_NAME', '')
+DB_PASSWORD = os.environ.get('DB_PASSWORD', '')
 
 DATABASES = {
     'default': {
